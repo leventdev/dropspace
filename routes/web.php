71f3035201c26f7ed9fcf4ebe7c599a51ac7ee4e
Kazó Levente <64219099+leventdev@users.noreply.github.com>
2022-04-03 19:34:16 +0200

    Remove CSRF for CLI uploads

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('upload');
 });
 
-Route::post('/upload-cli', 'App\Http\Controllers\FileUploadController@uploadCLIChunks');
+Route::post('/upload-cli', 'App\Http\Controllers\FileUploadController@uploadCLIChunks')->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
 Route::post('/upload-chunks', 'App\Http\Controllers\FileUploadController@uploadChunks');
 //Route to updating the expiry of a file
 Route::post('/update-expiry', 'App\Http\Controllers\FileDownloadController@updateExpiry');
