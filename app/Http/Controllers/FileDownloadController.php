5cf62e78efef02ee3c14ff3bf47615b0862511f4
Kazó Levente <64219099+leventdev@users.noreply.github.com>
2022-04-07 23:32:32 +0200

    Patch local stored downloads, change default storage type

--- app/Http/Controllers/FileDownloadController.php
+++ app/Http/Controllers/FileDownloadController.php
@@ -101,8 +101,8 @@ class FileDownloadController extends Controller
                     //If the hash of the password is correct, the file is returned
                     $file->download_count = $file->download_count + 1;
                     $file->save();
-                    if ($file->ds_storage_type == 'local') {
-                        return response()->download('dropspace/uploads/' . $file->path, $file->name);
+                    if (config('dropspace.ds_storage_type') == 'local') {
+                        return Storage::download('dropspace/uploads/' . $file->path, $file->name);
                     } else {
                         //return readfile(Storage::disk('s3')->temporaryUrl('dropspace/uploads/' . $file->path, Carbon::now()->addMinutes(5)), $file->name);
                         $tempFile = tempnam(sys_get_temp_dir(), $file->name);
@@ -117,8 +117,8 @@ class FileDownloadController extends Controller
                 //If the file is not protected, the file is returned
                 $file->download_count = $file->download_count + 1;
                 $file->save();
-                if ($file->ds_storage_type == 'local') {
-                    return response()->download('dropspace/uploads/' . $file->path, $file->name);
+                if (config('dropspace.ds_storage_type') == 'local') {
+                    return Storage::download('dropspace/uploads/' . $file->path, $file->name);
                 } else {
                     //return readfile(Storage::disk('s3')->temporaryUrl('dropspace/uploads/' . $file->path, Carbon::now()->addMinutes(5)), $file->name);
                     $tempFile = tempnam(sys_get_temp_dir(), $file->name);
--- config/dropspace.php
+++ config/dropspace.php
@@ -5,6 +5,6 @@ return [
 
     //The storage type to use. Use 'local' for local storage in storage/app. Use 's3' for Amazon S3 storage.
     //Using the 's3' option won't switch all storage operations to S3, some tasks (like chunked file uploads) will still be handled locally, and requires write permissions to the storage directory.
-    'ds_storage_type' => 's3',
+    'ds_storage_type' => 'local',
     'ds_email_enabled' => true,
 ];
