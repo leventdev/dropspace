bb42b130e806baa9153c145952628ca871f055e3
Kazó Levente <64219099+leventdev@users.noreply.github.com>
2022-06-05 21:57:09 +0000

    Remove CSRF for uploads and file details

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,7 @@ Route::get('/cli/domain-check', function ()
 {
     return 'Domain valid';
 });
-Route::post('/upload-cli', 'App\Http\Controllers\FileUploadController@uploadCLIChunks')->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
-Route::post('/upload-chunks', 'App\Http\Controllers\FileUploadController@uploadChunks');
+Route::post('/upload-chunks', 'App\Http\Controllers\FileUploadController@uploadChunks')->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
 //Route to updating the expiry of a file
 Route::post('/update-expiry', 'App\Http\Controllers\FileDownloadController@updateExpiry');
 //Route to uploading a file (Not the view, the actual file upload)
@@ -28,7 +27,7 @@ Route::post('/upload-file', 'App\Http\Controllers\FileUploadController@uploadFil
 //Route to setting file download limit, expiry date, password, etc.
 Route::get('/set-file-details/{file_id}', 'App\Http\Controllers\FileUploadController@setFileDetails');
 //Route to save the file's settings and save them to the database and return the file
-Route::post('/save-file-details/{file_id}', 'App\Http\Controllers\FileUploadController@saveFileDetails');
+Route::post('/save-file-details/{file_id}', 'App\Http\Controllers\FileUploadController@saveFileDetails')->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
 //Route to the file
 Route::get('/download-file/{file_id}', 'App\Http\Controllers\FileDownloadController@returnFile');
 //Route to downloading a file (The view, not the actual file)
