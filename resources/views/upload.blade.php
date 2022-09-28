c2382e4fb6193f35dd4dc7ba537d67a6480bd1b5
Leventdev <kazo.levente@gmail.com>
2022-09-28 14:08:34 +0200

    Implement DropSpace Chunker (Testing)

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -41,7 +41,6 @@
         }
 
         .button--loading::after {
-            content: "";
             position: absolute;
             width: 60px;
             height: 60px;
@@ -95,7 +94,7 @@
         }
     </style>
     <div class="bg-gray-800 min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
-    @if (config('dropspace.ds_security_enabled') && Auth::check())
+        @if (config('dropspace.ds_security_enabled') && Auth::check())
         <div class="absolute top-4 right-6 grid grid-cols-1 grid-rows-2">
             <div>
                 <button type="button" onclick="window.location.href='/settings/'" class="inline-flex justify-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gradient-to-r from-indigo-500 to-blue-500 hover:bg-gray-50 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
@@ -126,11 +125,18 @@
                     <div class="sm:ml-6 self-center sm:border-l sm:border-gray-200 sm:pl-6">
                         <div>
                             <div class="mx-auto max-w-xl transform rounded-xl bg-gray-600 p-2 shadow-2xl ring-1 ring-black ring-opacity-5 transition-all">
-                                <button type="button" id="upload-button" class="relative block min-w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                <button type="button" id="upload-button" class="relative block min-w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                                     <svg id="upload-icon" xmlns="http://www.w3.org/2000/svg" class="mx-auto h-12 w-12 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
                                     </svg>
-                                    <span class="mt-2 block text-sm font-medium text-gray-50"> Click to upload a file </span>
+                                    <svg id="success-icon" style="display: none;" xmlns="http://www.w3.org/2000/svg" class="mx-auto h-12 w-12 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+                                    <svg id="upload-spinner" style="display: none;" class="animate-spin text-gray-400 h-12 w-12 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                        <path class="opacity-100" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                    </svg>
+                                    <span id="upload-text" class="mt-2 block text-sm font-medium text-gray-50"> Click to upload a file </span>
                                 </button>
                                 <div style="display: none;" id="progress-message" class="mb-0 mt-2 text-lg font-medium dark:text-white">Uploading...</div>
                                 <div id="loader-big" style="display: none;" class="w-64 mt-1 bg-gray-200 rounded-full h-4 dark:bg-gray-500">
@@ -138,6 +144,7 @@
                                 </div>
                             </div>
                         </div>
+                        <input type="file" id="file-input" name="file" class="hidden" />
                         <div class="mt-4 relative grid grid-cols-1 items-stretch">
                             <button type="button" onclick="window.location.href='/sharecode/'" class="justify-center inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-gradient-to-r from-indigo-500 to-blue-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="-ml-1 mr-3 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
@@ -156,91 +163,192 @@
         AOS.init();
     </script>
 </body>
+
+<script>
+    //When the user clicks on the upload button, open the file open prompt.
+    document.getElementById('upload-button').onclick = function() {
+        document.getElementById('file-input').click();
+    }
+
+    //When a file is selected. Start upload
+    document.getElementById('file-input').onchange = async function() {
+        var fileInput = document.getElementById('file-input');
+        //calculate file size in MB
+        var fSize = fileInput.files[0].size / 1048576;
+        document.getElementById('file-input').disabled = true;
+        document.getElementById('loader-big').style.display = "block";
+        document.getElementById('progress-message').style.display = "block";
+        document.getElementById('upload-spinner').style.display = "block";
+        document.getElementById('upload-icon').style.display = 'none';
+        document.getElementById('upload-text').style.display = 'none';
+
+        var file = fileInput.files[0];
+        var chunkSize = 1024 * 1024 * 5; // 5MB
+        var fileSize = file.size;
+        var chunks = Math.ceil(fileSize / chunkSize, chunkSize) - 1;
+        var chunk = 0;
+
+        //Generate timestamp
+        var timestamp = Date.now();
+
+        var dropid = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + '-' + file.name;
+        console.log('Uploadig ' + dropid);
+
+        var statusToggle = false;
+        while (chunk <= chunks) {
+            var offset = chunk * chunkSize;
+            console.log('current chunk: ' + chunk);
+
+            var currentChunk = file.slice(offset, offset + chunkSize);
+            const formData = new FormData();
+            formData.append('file', currentChunk);
+            formData.append('chunk', chunk);
+            formData.append('chunks', chunks);
+            formData.append('filename', file.name);
+            formData.append('filesize', fileSize);
+            formData.append('_token', '{{ csrf_token() }}');
+            formData.append('dropid', dropid);
+            //Update with the route for the first step post call
+            const Url = "{{{ route('uploadChunk') }}}";
+            const response = await fetch(Url, {
+                method: 'POST',
+                body: formData
+            });
+
+            if (response.status == 200) {
+                chunk++;
+
+                var fileProgress = (chunk / chunks);
+                console.log(fileProgress);
+                if (fileProgress > 1) {
+                    fileProgress = 1;
+                }
+                document.getElementById('loader-progress').style.width = fileProgress * 100 + '%';
+                document.getElementById('loader-progress').innerText = Math.ceil(fileProgress * 100) + "%";
+
+                if (Math.ceil(fileProgress * 100) == 100) {
+                    setTimeout(function() {
+                        document.getElementById('loader-progress').classList.add("finished-animate");
+                        document.getElementById('progress-message').innerText = "Processing chunks...";
+                    })
+                }
+            } else {
+                document.getElementById('progress-message').innerText = "Upload failed. Please try again.";
+            }
+        }
+
+        const processForm = new FormData();
+        processForm.append('fileName', file.name);
+        processForm.append('totalChunks', chunks);
+        processForm.append('fileSize', fileSize);
+        processForm.append('dropid', dropid);
+        processForm.append('_token', '{{ csrf_token() }}');
+
+        //Update with the route for the last step post call
+        fetch("{{{ route('processChunks') }}}", {
+            method: 'POST',
+            body: processForm
+        }).then(response => {
+            if (response.status == 200) {
+                //Redirect to settings page.
+                //console.log(response.json()['status']);
+                //
+                //identifier = JSON.parse(response.json()).result.identifier;
+                //window.location.href = "{{url('set-file-details')}}/" + identifier;
+                response.json().then(result=>{
+                    window.location.href = "{{url('set-file-details')}}/" + result.identifier;
+                })
+            } else {
+                document.getElementById('progress-message').innerText = "Processing failed. Please try again.";
+            }
+        })
+    }
+</script>
 <script>
     /*
     document.getElementById('buttonid').addEventListener('click', function() {
         document.getElementById('fileid').click();
     });*/
+    /* 
+        var r = new Resumable({
+            target: "{{secure_url('upload-chunks')}}",
+            query: {
+                _token: '{{ csrf_token() }}'
+            },
+            simultaneousUploads: 3,
+            maxChunkRetries: 10,
+            chunkSize: 5000000,
+            forceChunkSize: true,
+        });
 
-    var r = new Resumable({
-        target: "{{secure_url('upload-chunks')}}",
-        query: {
-            _token: '{{ csrf_token() }}'
-        },
-        simultaneousUploads: 3,
-        maxChunkRetries: 10,
-        chunkSize: 5000000,
-        forceChunkSize: true,
-    });
+        r.assignBrowse(document.getElementById('upload-button'));
+        r.assignDrop(document.body);
 
-    r.assignBrowse(document.getElementById('upload-button'));
-    r.assignDrop(document.body);
+        document.body.addEventListener('paste', function(e) {
+            var item = e.clipboardData.items[0];
+            if (item.kind === 'file') {
+                var file = item.getAsFile();
+                r.addFile(file);
+            }
+        })
 
-    document.body.addEventListener('paste', function(e) {
-        var item = e.clipboardData.items[0];
-        if (item.kind === 'file') {
-            var file = item.getAsFile();
-            r.addFile(file);
-        }
-    })
+        // If r has file added, start uploading
+        r.on('fileAdded', function(file) {
+            r.upload();
+            const btn = document.getElementById('upload-button');
+            btn.classList.add("button--loading");
+            btn.innerText = "";
+            document.getElementById('loader-big').style.display = "block";
+            document.getElementById('progress-message').style.display = "block";
+            document.getElementById('upload-icon').style.display = 'none';
+        });
 
-    // If r has file added, start uploading
-    r.on('fileAdded', function(file) {
-        r.upload();
-        const btn = document.getElementById('upload-button');
-        btn.classList.add("button--loading");
-        btn.innerText = "";
-        document.getElementById('loader-big').style.display = "block";
-        document.getElementById('progress-message').style.display = "block";
-        document.getElementById('upload-icon').style.display = 'none';
-    });
+        var identifier;
 
-    var identifier;
+        r.on('fileSuccess', function(file, message) {
+            console.log(message);
+            //redirect to Route::get('/set-file-details/{file_id}'
+            //Make identifier the 'identifier' from the message
+            //Read message as json
 
-    r.on('fileSuccess', function(file, message) {
-        console.log(message);
-        //redirect to Route::get('/set-file-details/{file_id}'
-        //Make identifier the 'identifier' from the message
-        //Read message as json
+            identifier = JSON.parse(message).identifier;
+            window.location.href = "{{url('set-file-details')}}/" + identifier;
+        });
 
-        identifier = JSON.parse(message).identifier;
-        window.location.href = "{{url('set-file-details')}}/" + identifier;
-    });
+        r.on('fileError', function(file, message) {
+            Sentry.captureException(message);
+            const btn = document.getElementById('upload-button');
+            btn.classList.remove("button--loading");
+            const obj = JSON.parse(message);
+            if (message.includes('File size exceeds maximum file size.') || message.includes('Not enough space on server' || message.includes('Uploading requires you to be signed in'))) {
+                btn.innerHTML = '<span class="mt-2 block text-sm font-medium text-gray-50"> ' + obj.error + ' </span>';
+            } else {
+                btn.innerHTML = '<span class="mt-2 block text-sm font-medium text-gray-50"> Error while uploading file. Please try again. </span>';
+            }
+            document.getElementById('loader-big').style.display = "none";
+        });
 
-    r.on('fileError', function(file, message) {
-        Sentry.captureException(message);
-        const btn = document.getElementById('upload-button');
-        btn.classList.remove("button--loading");
-        const obj = JSON.parse(message);
-        if (message.includes('File size exceeds maximum file size.') || message.includes('Not enough space on server' || message.includes('Uploading requires you to be signed in'))) {
-            btn.innerHTML = '<span class="mt-2 block text-sm font-medium text-gray-50"> ' + obj.error + ' </span>';
-        } else {
-            btn.innerHTML = '<span class="mt-2 block text-sm font-medium text-gray-50"> Error while uploading file. Please try again. </span>';
-        }
-        document.getElementById('loader-big').style.display = "none";
-    });
+        var statusToggle = false;
 
-    var statusToggle = false;
+        r.on('fileProgress', (file, ratio) => {
+            document.getElementById('loader-progress').style.width = (file.progress() * 100) + "%";
+            //Get file progress with 0 decimal rounded up
 
-    r.on('fileProgress', (file, ratio) => {
-        document.getElementById('loader-progress').style.width = (file.progress() * 100) + "%";
-        //Get file progress with 0 decimal rounded up
+            document.getElementById('loader-progress').innerText = Math.ceil(file.progress() * 100) + "%";
+            //If 100% then add class
+            if (Math.ceil(file.progress() * 100) == 100) {
+                //Wait a second then add class finished-animte
+                setTimeout(function() {
+                    document.getElementById('loader-progress').classList.add("finished-animate");
+                    if (statusToggle == false) {
+                        document.getElementById('progress-message').innerText = "Assembling chunks...";
+                        statusToggle = true;
+                    } else {
 
-        document.getElementById('loader-progress').innerText = Math.ceil(file.progress() * 100) + "%";
-        //If 100% then add class
-        if (Math.ceil(file.progress() * 100) == 100) {
-            //Wait a second then add class finished-animte
-            setTimeout(function() {
-                document.getElementById('loader-progress').classList.add("finished-animate");
-                if (statusToggle == false) {
-                    document.getElementById('progress-message').innerText = "Assembling chunks...";
-                    statusToggle = true;
-                } else {
-
-                }
-            }, 350);
-        }
-    });
+                    }
+                }, 350);
+            }
+        }); */
 </script>
 
 </html>
